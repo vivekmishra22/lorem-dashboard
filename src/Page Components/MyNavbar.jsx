47903fd845d30e527365793d33cb0351a390e892
vivekmishra22 <vivekunofficialmail.com>
2024-12-20 13:10:14 +0530

    updated code

--- src/Page Components/MyNavbar.jsx
+++ src/Page Components/MyNavbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Col, Container, Row, Navbar, Nav, Image, Form, Offcanvas } from 'react-bootstrap';
+import { Col, Container, Row, Navbar, Image, Offcanvas, Dropdown } from 'react-bootstrap';
 import userLogo from '../Assets/Images/user-icon.webp';
+import { FaLaptopCode, FaUserFriends, FaShoppingCart } from "react-icons/fa";
+import { RxDashboard } from "react-icons/rx";
+import { TfiWrite } from "react-icons/tfi";
+import { AiOutlineForm } from "react-icons/ai";
+import { IoLogOut } from "react-icons/io5";
+import { FaTable } from "react-icons/fa6";
 
 const MyNavbar = () => {
     return (
@@ -9,54 +15,85 @@ const MyNavbar = () => {
                 <Row>
                     <Col md={2}>
                         <div>
-                            <Offcanvas show={true} backdrop={false} style={{ width: "17.5%" }}>
-                                <Offcanvas.Header>
-                                    <Offcanvas.Title>Admin Dashboard</Offcanvas.Title>
+                            <Offcanvas className="bg-dark text-light" show={true} backdrop={false} style={{ width: "17.5%" }}>
+                                <Offcanvas.Header className='d-flex justify-content-center align-items-center'>
+                                <FaLaptopCode className="me-2 fs-2 text-primary" />
+                                    <Offcanvas.Title className="fs-3">Admin Dashboard</Offcanvas.Title>
                                 </Offcanvas.Header>
+                                <hr className="m-0" />
                                 <Offcanvas.Body>
-                                    Some text as placeholder. In real life you can have the elements you
-                                    have chosen. Like, text, images, lists, etc.
+
+                                    <ul className="list-unstyled">
+                                        <li className='mb-2 d-flex align-items-center'>
+                                            <RxDashboard className="me-2 fs-5 text-info" />
+                                            <a className="text-decoration-none text-light" href="#dashboard">Dashboard</a>
+                                        </li>
+                                        <li className='mb-2 d-flex align-items-center'>
+                                            <FaUserFriends className="me-2 fs-5 text-info" />
+                                            <a className="text-decoration-none text-light" href="#user">User</a>
+                                        </li>
+                                        <li className='mb-2 d-flex align-items-center'>
+                                        <AiOutlineForm className="me-2 fs-5 text-info" />
+                                            <a className="text-decoration-none text-light" href="#form">Form</a>
+                                        </li>
+                                        <li className='mb-2 d-flex align-items-center'>
+                                        <FaTable className="me-2 fs-5 text-info" />
+                                            <a className="text-decoration-none text-light" href="#table">Table</a>
+                                        </li>
+                                        <li className='mb-2 d-flex align-items-center'>
+                                            <FaShoppingCart className="me-2 fs-5 text-info" />
+                                            <a className="text-decoration-none text-light" href="#product">Product</a>
+                                        </li>
+                                        <li className='mb-2 d-flex align-items-center mt-5'>
+                                            <IoLogOut className="me-2 fs-5 text-danger" />
+                                            <a className="text-decoration-none text-light" href="#logout">Logout</a>
+                                        </li>
+                                    </ul>
+
                                 </Offcanvas.Body>
+
                             </Offcanvas>
                         </div>
                     </Col>
 
                     <Col md={10}>
-                        <Navbar bg="primary" variant="dark" expand="lg">
-                            <Container>
+                        <Navbar bg="dark" variant="dark" expand="lg">
+                            <Container className="p-2">
                                 {/* Navbar Brand */}
                                 <Navbar.Brand href="#home">MyNavbar</Navbar.Brand>
 
-                                {/* Toggle Button */}
-                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                                {/* Collapsible Navigation Links */}
-                                <Navbar.Collapse id="basic-navbar-nav">
-                                    <Nav className="m-auto">
-                                        <Nav.Link href="#home">Home</Nav.Link>
-                                        <Nav.Link href="#about">About</Nav.Link>
-                                        <Nav.Link href="#contact">Contact</Nav.Link>
-                                    </Nav>
-                                </Navbar.Collapse>
-                                <div className='d-flex justify-content-center align-items-center'>
-                                    <Image
-                                        src={userLogo}
-                                        roundedCircle
-                                        width="40"
-                                        height="40"
-                                        className="ms-auto d-inline-block align-middle"
-                                        alt="User Logo"
-                                    />
-                                    <Form.Select className='bg-transparent border-0' aria-label="Default select example">
+
+                            </Container>
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <Image
+                                    src={userLogo}
+                                    roundedCircle
+                                    width="40"
+                                    height="40"
+                                    className="d-inline-block align-middle border border-info p-1"
+                                    alt="User Logo"
+                                />
+
+                                <Dropdown>
+                                    <Dropdown.Toggle className="bg-transparent border-0" variant="success" id="dropdown-basic">
+                                        user@gmail.com
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                {/* <Form.Select className='bg-transparent text-primary border-0' aria-label="Default select example">
                                         <option>user@gmail.com</option>
                                         <option value="1">One</option>
-                                        {/* <option value="2">Two</option>
-                                        <option value="3">Three</option> */}
-                                    </Form.Select>
-                                    {/* <Image src={userLogo} rounded /> */}
-                                </div>
-                                {/* <a href="">anchor</a> */}
-                            </Container>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select> */}
+                                {/* <Image src={userLogo} rounded /> */}
+                            </div>
+                            {/* <a href="">anchor</a> */}
                         </Navbar>
                     </Col>
                 </Row>
